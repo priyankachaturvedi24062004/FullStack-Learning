f8da139331b3779c0785c1485ace44216ba87ac3
priyankachaturvedi24062004 <priyankachaturvedi24062004@gmail.com>
2026-01-23 23:08:33 +0530

    Schema & Models

--- Mongo/index.js
+++ Mongo/index.js
@@ -9,3 +9,12 @@ main()
 async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/test');
 }
+
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    age: Number,
+    email: String,
+});
+
+const User = mongoose.model('User', userSchema);;
