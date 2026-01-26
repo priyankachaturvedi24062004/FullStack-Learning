cc3f6782159e9c7f36ece9ee080a831e1d0c894a
priyankachaturvedi24062004 <priyankachaturvedi24062004@gmail.com>
2026-01-26 17:28:43 +0530

    Initialize database

---
+++ Mongo3/init.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Chat = require('./models/chat.js');
+
+main()
+.then(() => console.log("connection successfull"))
+.catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
+}
+
+let allChats = [
+    {
+    from : "Taslima",
+    to : "priyanka",
+    msg : "Hello priyanka! This is Taslima.",  
+    created_at : new Date()
+    },
+    {
+    from : "Eve",
+    to : "Tiya",
+    msg : "What are you doing!",  
+    created_at : new Date()
+    },
+    {
+    from : "Tom",
+    to : "Cat",
+    msg : "Hello Cat! This is Tom.",  
+    created_at : new Date()
+    },
+]
+
+Chat.insertMany (allChats);
