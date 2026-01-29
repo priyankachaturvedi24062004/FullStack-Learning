0bdeccb119c8221f40d266b1fd698e3de830e4cb
priyankachaturvedi24062004 <priyankachaturvedi24062004@gmail.com>
2026-01-29 23:21:18 +0530

    Update Route

--- Mongo3/index.js
+++ Mongo3/index.js
@@ -3,11 +3,13 @@ const app = express();
 const mongoose = require('mongoose');
 const path = require('path');
 const Chat = require('./models/chat.js');
+const methodOverride = require('method-override');
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
 
 
 main()
@@ -23,7 +25,7 @@ async function main() {
 //Index route
 app.get("/", async(req, res) => {
   let chats = await Chat.find()
-  console.log(chats);
+  //console.log(chats);
   res.render("index.ejs", {chats});
 });
 
@@ -60,6 +62,22 @@ app.get("/chats/:id/edit", async(req, res) => {
     let chat = await Chat.findById(id);
     res.render("edit.ejs",{chat});
 });
+
+//update chat route
+app.put("/chats/:id",async (req, res) => {
+    let { id } = req.params;
+    let { msg: newMsg } = req.body;
+    console.log(newMsg);
+    let chat = await Chat.findByIdAndUpdate(
+        id, 
+        {msg: newMsg},
+        {runValidators: true, new: true}
+);
+
+    console.log("updatedChat");
+    res.redirect("/");
+});
+
 
 app.listen(8080,() =>{
     console.log("server is listening on port 8080");
