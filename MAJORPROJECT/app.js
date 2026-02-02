b2c23280e9da65c09113c37b35ecbb296eab1cda
priyankachaturvedi24062004 <priyankachaturvedi24062004@gmail.com>
2026-02-02 18:13:56 +0530

    Index Route

--- MAJORPROJECT/app.js
+++ MAJORPROJECT/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const Listing = require('./models/listing.js');
+const path = require('path');
 
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
@@ -19,11 +20,20 @@ async function main(){
     await mongoose.connect(MONGO_URL);
 }
 
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"/views"));
+
 app.get("/", (req, res) => {
     res.send("Hi, I am root")
 });
 
-app.get("/testListing", async (req, res) => {
+app.get("/listings", async (req, res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index", { allListings });
+
+});
+
+/*app.get("/testListing", async (req, res) => {
     let sampleListing = new Listing({
         title:"My New Villa",
         description:" By the beach",
@@ -34,7 +44,7 @@ app.get("/testListing", async (req, res) => {
     await sampleListing.save()
     console.log("sample was saved");
     res.send("successful testing");
-});
+});*/
 
 app.listen(8080,() => {
     console.log("Server is running on port 8080");
--- MAJORPROJECT/models/listing.js
+++ MAJORPROJECT/models/listing.js
@@ -1,26 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
-    title: {
-        type:String,
-        required: true,
-    },
-    description: String,
-    image: {
-        type:String,
-        default:
-            "https://unsplash.com/photos/silhouette-of-palm-tree-near-body-of-water-during-sunset-CXyz3qljaH8",
-        set: (v) => 
-            v === "" 
-            ? "https://unsplash.com/photos/silhouette-of-palm-tree-near-body-of-water-during-sunset-CXyz3qljaH8"
-            : v,
-    },
-    price: Number,  
-    location: String,
-    country: String,
-    
+  title: String,
+  description: String,
+  image: {
+    filename: String,
+    url: String
+  },
+  price: Number,
+  location: String,
+  country: String
 });
 
-const Listing = mongoose.model('Listing', listingSchema);
-module.exports = Listing;
+module.exports = mongoose.model("Listing", listingSchema);
