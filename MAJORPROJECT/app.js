b31e5f481d8f9987c4f802cfa3cda1cd14ee4e9e
priyankachaturvedi24062004 <priyankachaturvedi24062004@gmail.com>
2026-02-03 23:39:39 +0530

    DELETE(delete route)

--- MAJORPROJECT/app.js
+++ MAJORPROJECT/app.js
@@ -57,6 +57,13 @@ app.put("/listings/:id", async (req, res) => {
     res.redirect(`/listings/${id}`);
 });
 
+//Delete Route
+app.delete("/listings/:id", async (req, res) => {
+    let { id } = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    res.redirect("/listings");
+});
 
 //Show Route
 app.get("/listings/:id", async (req, res) => {
