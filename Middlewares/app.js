0064a05b5604e8d9b5fed8516eb2d07b2530cba3
priyankachaturvedi24062004 <priyankachaturvedi24062004@gmail.com>
2026-02-14 23:22:06 +0530

    API token as query string

--- Middlewares/app.js
+++ Middlewares/app.js
@@ -13,16 +13,28 @@ app.use((req, res, next) => {
 }); */
 
 app.use((req, res, next) => {
+    let {token} = req.query;
+    if (token === 'giveaccess') {
+        next();
+    }
+    res.send("ACCESS DENIED!");
+});
+
+app.use((req, res, next) => {
     console.log('I am only for random');
     next();
 });
 
-//logger
+app.get("/api", (req, res) => {
+    res.send('data');
+});
+
+/*logger
 app.use((req, res, next) => {
     req.time = new Date(Date.now()).toString();
     console.log(req.method, req.hostname, req.path, req.time);
     next();
-});
+});*/
 
 app.get('/', (req, res) => {
     res.send('Hi,I am root.');
@@ -36,6 +48,8 @@ app.get("/random", (req, res) => {
 app.use((req, res) => {
     res.status(404).send('Page Not Found!');
 });
+
+
 
 app.listen(8080, () => {
     console.log('Server is running on port 8080');
