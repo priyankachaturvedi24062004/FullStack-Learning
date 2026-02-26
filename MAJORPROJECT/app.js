3ef40c3b306b02c0651bded08476514fb751d711
priyankachaturvedi24062004 <priyankachaturvedi24062004@gmail.com>
2026-02-26 18:53:33 +0530

    Custom Error Handling

--- MAJORPROJECT/app.js
+++ MAJORPROJECT/app.js
@@ -89,13 +89,14 @@ app.get("/listings/:id", async (req, res) => {
 });
 
 //Create Route
-app.post("/listings", async (req, res) => {
-    let newListing = new Listing(req.body.listing);
-
-    newListing.image.filename = "listingimage";  // temporary static filename
-
-    await newListing.save();
-    res.redirect("/listings/");
+app.post("/listings", async (req, res, next) => {
+    try{
+        const newListing = new Listing(req.body.listing);
+        await newListing.save();
+        res.redirect("/listings/");
+    }catch(err){
+        next(err);
+    }
 });
 
 /*app.get("/testListing", async (req, res) => {
@@ -110,6 +111,10 @@ app.post("/listings", async (req, res) => {
     console.log("sample was saved");
     res.send("successful testing");
 });*/
+
+app.use((err, req, res, next) => {
+    res.send("Something went wrong");
+});
 
 app.listen(8080,() => {
     console.log("Server is running on port 8080");
