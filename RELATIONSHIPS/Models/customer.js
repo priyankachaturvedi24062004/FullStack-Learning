ec82e03fa7fbca83cf02d58272fc4156e9528391
priyankachaturvedi24062004 <priyankachaturvedi24062004@gmail.com>
2026-03-09 20:48:18 +0530

    Using populate

--- RELATIONSHIPS/Models/customer.js
+++ RELATIONSHIPS/Models/customer.js
@@ -26,22 +26,16 @@ const customerSchema = new Schema({
 const Order = mongoose.model('Order', orderSchema);
 const Customer = mongoose.model('Customer', customerSchema);
 
-const addCustomer = async() => {
-    let cust1 = new Customer({
-        name:"Rahul Kumar"
-    });
-    
-    let order1 = await Order.findOne({item: "Kachori"});
-    let order2 = await Order.findOne({item: "Pav Bhaji"});
+const findCustomer = async() => {
 
-    cust1.orders.push(order1);
-    cust1.orders.push(order2);
 
-    let result = await cust1.save();
-    console.log(result);
+    let result = await Customer.find({}).populate("orders");
+    console.log(result[0]);
+
 };
 
-addCustomer();
+
+findCustomer();
 
 /*const addOrders = async() => {
     let res = await Order.insertMany([
